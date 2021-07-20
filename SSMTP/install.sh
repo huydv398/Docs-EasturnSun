b548d609d7a01dd1bd3a03f61537b367d939fec7
huydv <61343215+huydv398@users.noreply.github.com>
2021-07-20 10:24:36 +0700

    Create install.sh

---
+++ SSMTP/install.sh
@@ -0,0 +1,48 @@
+#!/bin/bash 
+
+# Check OS
+cat /etc/os-release* |grep 'ubuntu' > /dev/null 2>&1 && OS='Ubuntu'
+cat /etc/os-release* |grep 'centos' > /dev/null 2>&1 && OS='CentOS' 
+echo $OS
+
+# install_mariadb
+
+if [ "$OS" == "CentOS" ]
+then
+    echo centos
+elif [ "$OS" == "Ubuntu" ]
+then
+    echo U18
+else
+    echo none
+fi
+
+cp /etc/ssmtp/ssmtp.conf /etc/ssmtp/ssmtp-$(date).conf.bak
+
+read -p "SMTP server:(Enter for: smtp.gmail.com): " smtpserver
+read -p "SMTP Username (Ex: user@gmail.com): " smtpuser
+read -p "SMTP Password (password for $smtpuser): " smtppassword
+read -p "SMTP port:(Enter for: 587): " smtpport
+if [ $smtpserver="" ]
+then 
+    smtpserver=smtp.gmail.com
+fi 
+
+if [ $smtpport="" ]
+then 
+    smtpport=587
+fi 
+echo "
+root=$smtpuser
+mailhub=smtp.gmail.com:587
+AuthUser=$smtpuser
+AuthPass=$smtppassword
+UseTLS=YES
+UseSTARTTLS=YES
+rewriteDomain=gmail.com
+FromLineOverride=YES
+" > /etc/ssmtp/ssmtp.conf
+
+echo " 
+root:$smtpuser:smtp.gmail.com:587
+" >> /etc/ssmtp/revaliases
